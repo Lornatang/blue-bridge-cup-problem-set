4816c587559ee0f2fec421d71da65a645305f943
Lornatang <shiyipaisizuo@gmail.com>
2018-12-19 15:36:12 +0800

    update codestyle and change code

--- 9th/C/4/main.cc
+++ 9th/C/4/main.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 /**
  * 标题：测试次数
 
@@ -19,31 +21,40 @@ x星球有很多高耸入云的高塔，刚好可以用来做耐摔测试。塔�
 注意：需要填写的是一个整数，不要填写任何多余内容。
 */
 
-#include <iostream>
-using namespace std;
-
 #define Max(a, b) (a > b ? a : b)
 #define Min(a, b) (a < b ? a : b)
 
-void dynamic_programming() {
-  int dp[1000][3];
-  int n, m;
-  scanf("%d%d", &n, &m);
-  for (int i = 1; i <= n; i++) {
-    dp[i][1] = i;
-  }
-  for (int cnt = 2; cnt <= m; cnt++) {
-    for (int ind = 1; ind <= n; ind++) {
-      dp[ind][cnt] = 1 + dp[ind - 1][cnt];
-      for (int k = 2; k <= ind; k++)
-        dp[ind][cnt] =
-            Min(dp[ind][cnt], 1 + Max(dp[k - 1][cnt - 1], dp[ind - k][cnt]));
-    }
-  }
-  printf("%d\n", dp[n][m]);
+/**
+ * The commonly used dynamic programming algorithm is adopted
+ * para:
+ *        n:numbers of floors.
+ *        m:numbers of phones.
+ * return:None
+ */
+void dynamic_programming(int n, int m) {
+  int dp[1001][1001];
+  //  layer by layer test
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++) dp[i][j] = i;
+  //  if it gets damaged at level i
+  for (int i = 2; i <= n; i++)
+    for (int j = 2; j <= m; j++)
+      for (int k = 2; k < i; k++)
+        dp[i][j] = min(max(dp[i - k][j] + 1, dp[k - 1][j - 1] + 1), dp[i][j]);
+  // for (int cnt = 2; cnt <= m; cnt++)
+  //   for (int ind = 1; ind <= n; ind++) {
+  //     //  dp[1][2] = 1 + dp[0][2]
+  //     //  dp[2][2] = 1 + (1 + dp[1][2])...
+  //     dp[ind][cnt] = 1 + dp[ind - 1][cnt];
+
+  //     for (int k = 2; k <= ind; k++)
+  //       dp[ind][cnt] =
+  //           Min(dp[ind][cnt], 1 + Max(dp[k - 1][cnt - 1], dp[ind - k][cnt]));
+  //   }
+  cout << dp[n][m] << "\n";
 }
 
 int main() {
-  dynamic_programming();
+  dynamic_programming(1000, 3);
   return 0;
 }
