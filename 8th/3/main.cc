8b98d374fc2b7f25921ffa74174e2cdd4c4e9a43
Lornatang <shiyipaisizuo@163.com>
2019-02-18 21:17:44 +0800

    init

---
+++ 8th/3/main.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double line[30][30];
+  double min_n, max_n;
+  int i, j;
+
+  freopen("dat.txt", "r", stdin);
+  for (i = 1; i < 30; i++)
+    for (j = 1; j <= i; j++) cin >> line[i][j];
+
+  for (i = 0; i < 30; i++)
+    for (j = 0; j < i; j++) {
+      line[i + 1][j] += line[i][j] / 2;
+      line[i + 1][j + 1] += line[i][j] / 2;
+    }
+
+  max_n = min_n = line[30][1];
+
+  for (i = 0; i < 30; i++) {
+    if (max_n > line[30][i]) max_n = line[30][i];
+    if (min_n < line[30][i]) max_n = line[30][i];
+  }
+
+  printf("n=m== %lf %lf\n", min_n, max_n);
+
+  printf("%lf\n", min_n * (long long)2086458231 / max_n);
+
+  return 0;
+}
