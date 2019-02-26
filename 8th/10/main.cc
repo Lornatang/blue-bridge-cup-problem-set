8508d473afcc561b05397984a3fed2f0e95f88cb
Lornatang <shiyipaisizuo@163.com>
2019-02-26 15:48:22 +0800

    Update main.cc

--- 8th/10/main.cc
+++ 8th/10/main.cc
@@ -7,17 +7,24 @@ ll bk[100010] = {0};
 ll arr[1000010];
 
 int main() {
+  /*
+  前缀和
+  */
+  int sum[100001];  // sum[i] 前i个元素的和
+  int num[100001];
+  int cnt[100001];
   int n, k;
+  ll ans = 0;
+  memset(cnt, 0, sizeof(cnt));
+  memset(sum, 0, sizeof(sum));
   scanf("%d%d", &n, &k);
-  for (int i = 0; i < n; i++) scanf("%lld", &arr[i]);
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &num[i]);
+    sum[i] = (sum[i - 1] + num[i]) % k;
+    ans += cnt[sum[i]];
+    cnt[sum[i]]++;
+  }
 
-  arr[0] %= k;
-  ll sum = 0;
-
-  for (int i = 1; i < n; i++) arr[i] = (arr[i] + arr[i - 1]) % k;
-
-  for (int i = 0; i < n; i++) sum += (bk[arr[i]]++);
-
-  printf("%lld\n", sum + bk[0]);
+  printf("%lld\n", ans + cnt[0]);
   return 0;
 }
