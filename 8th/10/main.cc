fdb7538084ab4603e4fa81c869679eaffbeced28
Lornatang <shiyipaisizuo@163.com>
2019-02-26 14:11:41 +0800

    Create main.cc

---
+++ 8th/10/main.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+ll bk[100010] = {0};
+ll arr[1000010];
+int main() {
+  int n, k;
+  scanf("%d%d", &n, &k);
+  for (int i = 0; i < n; i++) scanf("%lld", &arr[i]);
+
+  arr[0] %= k;
+  ll sum = 0;
+
+  for (int i = 1; i < n; i++) arr[i] = (arr[i] + arr[i - 1]) % k;
+
+  for (int i = 0; i < n; i++) sum += (bk[arr[i]]++);
+
+  printf("%lld\n", sum + bk[0]);
+  return 0;
+}
