48ea3178c0b547a26672c165975a41f04d97e024
Lornatang <shiyipaisizuo@163.com>
2019-03-16 06:52:55 +0800

    up

---
+++ 7th/3/main.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <algorithm>
+
+int ans = 0;
+int num[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+bool visit[9];
+
+void solve() {
+  double sum = (double)num[0] + (double)num[1] / num[2] +
+               (double)(num[3] * 100 + num[4] * 10 + num[5]) /
+                   (num[6] * 100 + num[7] * 10 + num[8]);
+
+  if (sum == 10) ans++;
+}
+
+int main() {
+  while (std::next_permutation(num, num + 9)) solve();
+  printf("%d\n", ans);
+  return 0;
+}
