856ee48437a6410d718755df5f2f4f909b60b9c1
Lornatang <shiyipaisizuo@163.com>
2019-03-19 10:08:43 +0800

    Update two_long_numbers_add.cc

--- Test/easy/two_long_numbers_add.cc
+++ Test/easy/two_long_numbers_add.cc
@@ -1,28 +1,34 @@
 #include <iostream>
 using namespace std;
 
+string Add(string a, string b) {
+  while (a.size() > b.size()) b = '0' + b;
+  while (b.size() > a.size()) a = '0' + a;
+
+  a = '0' + a;
+  b = '0' + b;
+
+  for (int i = a.size() - 1; i >= 0; i--) {
+    int num1 = a[i] - '0';
+    int num2 = b[i] - '0';
+
+    if (num1 + num2 > 9) {
+      a[i - 1] = a[i - 1] - '0' + 1 + '0';
+      a[i] = (num1 + num2) - 10 + '0';
+    } else
+      a[i] = (num1 + num2) + '0';
+  }
+
+  if (a[0] == '0') a.erase(0, 1);
+
+  return a;
+}
+
 int main(void) {
   string s1, s2;
   cin >> s1 >> s2;
-
-  while (s1.size() > s2.size()) s2 = '0' + s2;
-  while (s1.size() < s2.size()) s1 = '0' + s1;
-
-  s1 = '0' + s1;
-  s2 = '0' + s2;
-
-  for (int i = s1.size() - 1; i >= 0; i--) {
-    int num1 = s1[i] - '0';
-    int num2 = s2[i] - '0';
-
-    if (num1 + num2 > 9) {
-      s1[i - 1] = s1[i - 1] - '0' + 1 + '0';
-      s1[i] = (num1 + num2) - 10 + '0';
-    } else
-      s1[i] = (num1 + num2) + '0';
-  }
-  if (s1[0] == '0') s1.erase(0, 1);
-  cout << s1 << "\n";
-
+  string res;
+  res = Add(s1, s2);
+  cout << res << "\n";
   return 0;
 }
