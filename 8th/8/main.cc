2b396848a86e150f739457dbdd17657835faa452
Lornatang <shiyipaisizuo@163.com>
2019-03-23 20:48:18 +0800

    Update main.cc

--- 8th/8/main.cc
+++ 8th/8/main.cc
@@ -1,46 +1,34 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-bool judge(int x, int y) {
-  while (y > 0) {
-    int tmp = x % y;
-    x = y;
-    y = tmp;
-  }
-  if (x == 1) return true;
-  return false;
-}
+const int maxm = 100010;
+const int maxn = 110;
+int a[maxn];
+int dp[maxm];
+int cnt = 0;
 
-int a[110], n;
-bool dp[10110];
+int gcd(int x, int y) { return !y ? x : gcd(y, x % y); }
 
-int main() {
-  cin >> n;
-  for (int i = 0; i < n; i++) cin >> a[i];
-  bool flag = false;
-  for (int i = 0; i < n; i++) {
-    for (int j = 1; j <= n; j++) {
-      if (judge(a[i], a[j])) {
-        flag = true;
-        break;
-      }
-    }
-    if (flag == 1) break;
+int main(void) {
+  int n;
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+
+  int k = gcd(a[1], a[2]);
+  for (int i = 3; i <= n; i++) k = gcd(k, a[i]);
+
+  if (!k)
+    printf("INF\n");
+  else {
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+      for (int j = a[i]; j < maxm; j++)
+        if (dp[j - a[i]] == 1) dp[j] = 1;
+
+    for (int i = 1; i <= 20000; i++)
+      if (dp[i] != 1) cnt += 1;
+
+    printf("%d\n", cnt);
   }
 
-  if (flag != 1) {
-    cout << "INF\n";
-    return 0;
-  }
-  dp[0] = 1;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j + a[i] < 10000; j++)
-      if (dp[j]) dp[j + a[i]] = 1;
-  }
-  int ans = 0;
-  for (int i = 0; i < 10000; i++) {
-    if (dp[i] != 1) ans++;
-  }
-  cout << ans << "\n";
   return 0;
 }
